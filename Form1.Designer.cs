2a4185c88d71b18965118a56aa5b6fd8664bd6d5
marco <marco@LAPTOPDIFONTANA>
2025-04-16 14:34:24 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿using System;
+
+namespace RocketArm_3._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar1.Location = new System.Drawing.Point(43, 60);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(844, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 90;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.Color.OrangeRed;
+            this.trackBar2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar2.Location = new System.Drawing.Point(43, 185);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar2.Maximum = 180;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(844, 56);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar2.Value = 90;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.trackBar3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar3.Location = new System.Drawing.Point(43, 321);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar3.Maximum = 0;
+            this.trackBar3.Minimum = -180;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(844, 56);
+            this.trackBar3.TabIndex = 2;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar3.Value = -90;
+            this.trackBar3.Scroll += new System.EventHandler(this.TrackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.BackColor = System.Drawing.Color.OrangeRed;
+            this.trackBar4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar4.Location = new System.Drawing.Point(43, 433);
+            this.trackBar4.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar4.Maximum = 0;
+            this.trackBar4.Minimum = -180;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(844, 56);
+            this.trackBar4.TabIndex = 3;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar4.Value = -90;
+            this.trackBar4.Scroll += new System.EventHandler(this.TrackBar4_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(881, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 55);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BASE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseCompatibleTextRendering = true;
+            this.label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(881, 185);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 55);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SNODO1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(881, 321);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 55);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SNODO2";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(881, 433);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 55);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SNODO3";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(881, 555);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 55);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "PINZA";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.trackBar5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar5.Location = new System.Drawing.Point(43, 555);
+            this.trackBar5.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar5.Maximum = 0;
+            this.trackBar5.Minimum = -180;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(844, 56);
+            this.trackBar5.TabIndex = 8;
+            this.trackBar5.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar5.Value = -90;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1041, 60);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1183, 60);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 25);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "invia";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1041, 185);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 55);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Demo 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1319, 59);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1319, 185);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 55);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Registra";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1023, 304);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(635, 306);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1023, 628);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 28);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1558, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1526, 628);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1673, 671);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+namespace RocketArm_3._0
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort arduinoPort;
+        public int[,] m;
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Scroll += trackBar1_Scroll_1;
+            trackBar2.Scroll += TrackBar2_Scroll;
+            trackBar3.Scroll += TrackBar3_Scroll;
+            trackBar4.Scroll += TrackBar4_Scroll;
+            trackBar5.Scroll += trackBar5_Scroll;
+            trackBar1.Value = trackBar2.Value = 90;
+            trackBar3.Value = -90;
+            trackBar4.Value = -90;
+            dataGridView1.Rows.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedItem = comboBox1.Items[0];
+
+            if (dataGridView1.Columns.Count == 0)
+            {
+                dataGridView1.Columns.Add("Col1", "TrackBar 1");
+                dataGridView1.Columns.Add("Col2", "TrackBar 2");
+                dataGridView1.Columns.Add("Col3", "TrackBar 3");
+                dataGridView1.Columns.Add("Col4", "TrackBar 4");
+                dataGridView1.Columns.Add("Col5", "TrackBar 5");
+            }
+            int a1 = trackBar1.Value, a2 = trackBar2.Value, a3 = trackBar3.Value, a4 = trackBar4.Value, a5 = trackBar5.Value;
+            dataGridView1.Rows.Add(a1, a2, a3, a4, a5);
+            m = createMatrix();
+        }
+        private void trackBar1_Scroll_1(object sender, EventArgs e)
+        {
+            SendAngle("a", trackBar1.Value);
+        }
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+            SendAngle("b", trackBar2.Value);
+        }
+        private void TrackBar3_Scroll(object sender, EventArgs e)
+        {
+            SendAngle("c", -trackBar3.Value);
+        }
+        private void TrackBar4_Scroll(object sender, EventArgs e)
+        {
+            SendAngle("d", -trackBar4.Value);
+        }
+        private void SendAngle(string command, int angle)   //invio dati
+        {
+            if (arduinoPort != null && arduinoPort.IsOpen)
+            {
+                try
+                {
+                    arduinoPort.WriteLine($"{command}{angle}");
+                }
+                catch (TimeoutException ex)
+                {
+                    MessageBox.Show("Tempo scaduto per inviare il comando: " + ex.Message);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Errore di I/O durante l'invio del comando: " + ex.Message);
+                }
+            }
+            else
+                MessageBox.Show("La porta seriale non è aperta.");
+        }
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            SendAngle("e", -trackBar5.Value);
+        }
+        private void button1_Click(object sender, EventArgs e)  //collegamento all'arduino
+        {
+            string com = (string)comboBox1.SelectedItem;
+            if (com != "")
+            {
+                arduinoPort = new SerialPort
+                {
+                    PortName = com,
+                    BaudRate = 9600,
+                    DataBits = 8,
+                    Parity = Parity.None,
+                    StopBits = StopBits.One
+                };
+            }
+            try
+            {
+                arduinoPort.Open();
+                MessageBox.Show("connected");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection error to Arduino: " + ex.Message);
+            }
+            comboBox1.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)  //demo
+        {
+            SendAngle("f", 0);
+        }
+        private void button3_Click(object sender, EventArgs e)  //registra valori
+        {
+            int a1 = trackBar1.Value, a2 = trackBar2.Value, a3 = trackBar3.Value, a4 = trackBar4.Value, a5 = trackBar5.Value;
+            dataGridView1.Rows.Add(a1, a2, a3, a4, a5);
+            m = createMatrix();
+        }
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)    //invio riga scelta
+        {
+            IList l = dataGridView1.CurrentRow.Cells;
+            int i = 97;
+            foreach (DataGridViewTextBoxCell o in l)
+            {
+                SendAngle((char)i++ + "", (int)o.Value);
+            }
+        }
+        private int[,] createMatrix()   //creazione 
+        {
+            IList l = dataGridView1.Rows;
+            int[,] m = new int[dataGridView1.RowCount, dataGridView1.ColumnCount];
+            for (int j = 0; j < dataGridView1.RowCount; j++)
+                for (int k = 0; k < dataGridView1.ColumnCount; k++)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[j];
+                    int? v = (int?) row.Cells[k].Value;
+                    if (v == null)
+                    {
+                        dataGridView1.Rows.RemoveAt(j);
+                        break;
+                    }
+                    else
+                        m[j, k] = (int)v;
+                }
+            return m;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < dataGridView1.RowCount; j++)
+                for (int k = 0; k < dataGridView1.ColumnCount; k++)
+                    SendAngle((char)(k + 97) + "", m[j, k]);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (arduinoPort != null && arduinoPort.IsOpen)
+                try
+                {
+                    if (arduinoPort.BytesToRead > 0)
+                    {
+                        string data = arduinoPort.ReadLine();
+                        label6.Text = $"Data received: {data}"; // Optional: Display the received data  
+                    }
+                }
+                catch (Exception ex)
+                {
+                MessageBox.Show("Error reading from the serial port: " + ex.Message);
+                }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+    }
+}
